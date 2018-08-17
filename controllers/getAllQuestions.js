1e3192a43042f8c147ebb42cdde1ea36b3a6bad9
Fejiroofficial <houseofjiro@gmail.com>
2018-08-17 14:38:48 +0100

    Ch(Tests for api end points) This is a commit for writing tests using mocha and chai

--- controllers/getAllQuestions.js
+++ controllers/getAllQuestions.js
@@ -1,6 +1,9 @@
 import { allQuestions } from '../datastore/questions';
 
 const getAllQuestions = (req, res) => {
+    if (allQuestions.length < 1){
+        return allQuestions;
+    }
     res.status(200).json ({
         status: "success",
        questions: allQuestions
