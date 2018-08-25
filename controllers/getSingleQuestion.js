74eb35dd94ce6e676c6f69d81e0184374c864998
Fejiroofficial <houseofjiro@gmail.com>
2018-08-25 21:11:48 +0100

    ft(API end point for fetching a specific question) This is a commit for fetching a specific question end point with answers.
    [Delivers #160037684]

--- controllers/getSingleQuestion.js
+++ controllers/getSingleQuestion.js
@@ -1,20 +1,24 @@
-import { allQuestions } from '../datastore/questions';
-import db from '../datastore/helperFn';
+import db from '../db';
 
 const getSingleQuestion = (req, res) => {
-    const questionId = parseInt(req.params.id, 10);
-    const questionFound = db.findOne(allQuestions, questionId);
-    
-    if (questionFound.length > 0) {
-       return res.status(200).json ({
-            status: "success",
-           questions: questionFound[0]
-        });
-    }
-    return res.status(404).json ({
-        status: "fail",
-        message: "Question not found"
-    });
-}
+  const question_id = parseInt(req.params.id, 10);
+  db.tx(t => {
+    const q1 = t.questions.findById(question_id);
+    const q2 = t.answers.all(question_id);
+    return t.batch([q1, q2]);
+  })
+    .then((result) => {
+      res.status(200).json({
+        status: "successful",
+        question: result,
+      });
 
+    })
+    .catch(err => {
+      return res.status(404).json({
+        status: "fail",
+        message: "question not found"
+      })
+    })
+};
 export default getSingleQuestion;
