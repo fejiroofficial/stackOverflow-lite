6c745e868241499e97d8259a5f455081ac0b47d0
Fejiroofficial <houseofjiro@gmail.com>
2018-09-06 13:22:19 +0100

    Installing cors to allow origin sharing

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import router from './routes';
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
 
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/v1', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
     success: "true",
     message: "Welcome to home page"
